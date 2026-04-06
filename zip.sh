d925409dcc2e6e9721447cf4b0f5e2d136fc85ed
GJ Tiquia <gjtiquia@gmail.com>
2026-04-06 23:06:45 +0800

    zip

---
+++ zip.sh
@@ -0,0 +1,8 @@
+# creating a zip archive from a directory [r]ecursively
+zip -r archive.zip directory_name
+
+# extract to current directory
+unzip path/to/archive.zip
+
+# extract to specific [d]irectory
+unzip path/to/archive.zip -d path/to/output
