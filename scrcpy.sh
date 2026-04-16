de132fec418aaa073f8ec781b2ddbe9d913beb6d
GJ Tiquia <gjtiquia@gmail.com>
2026-04-16 12:57:24 +0800

    more comments

--- scrcpy.sh
+++ scrcpy.sh
@@ -4,4 +4,5 @@ scrcpy
 
 # screen copy
 # Screen recording with scrcpy
+# this command is usable as is, it automatically sets the date in the correct format, no edit required
 scrcpy --no-audio --record "screen-recording_$(date +"%Y-%m-%d_%H.%M.%S").mp4"
