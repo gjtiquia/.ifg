430ccb309a530ed52a6e3d5a6b438fb3b86c237f
GJ Tiquia <gjtiquia@gmail.com>
2026-04-16 12:56:32 +0800

    scrcpy

--- scrcpy.sh
+++ scrcpy.sh
@@ -1,7 +1,7 @@
+# screen copy
 # mirror android to computer
 scrcpy
 
+# screen copy
 # Screen recording with scrcpy
 scrcpy --no-audio --record "screen-recording_$(date +"%Y-%m-%d_%H.%M.%S").mp4"
-
-
