d3c3b4f15b0efc090f911e552d787ba935dec2ac
GJ Tiquia <gjtiquia@gmail.com>
2026-07-31 13:54:13 -0400

    exiftool timezone

--- misc.sh
+++ misc.sh
@@ -16,15 +16,20 @@ SetFile -d "10/25/2023 15:30:45" *
 SetFile -m "10/25/2023 15:30:45" *
 
 # photo metadata set date - step 1
-# set all photos in current directory to the same starting time, including timezone at the end
+# set all photos in current directory to the same starting time
 exiftool -AllDates="2023:10:25 15:00:00+08:00" -overwrite_original .
 
 # photo metadata set date - step 2
+# set timezone
+exiftool -OffsetTimeOriginal="+08:00" -OffsetTimeDigitized="+08:00" -overwrite_original .
+
+# photo metadata set date - step 3
 # set time by one second forward for each photo in filename order
 exiftool '-AllDates+<0:0:$filesequence' -FileOrder Filename -overwrite_original .
 
-# photo metadata set date - step 3
+# photo metadata set date - step 4
 # set last created and last modified dates
+# note: this does not copy the timezone unfortunately... should look into it # TODO
 exiftool "-FileCreateDate<DateTimeOriginal" "-FileModifyDate<DateTimeOriginal" -overwrite_original .
 
 # create folder / directory with current date and time
